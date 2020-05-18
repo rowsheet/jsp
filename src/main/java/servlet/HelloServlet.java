ddff6b2d26b103168c848d80effe9400486e865b
Alexander Kleinhans <rowsheet.com@gmail.com>
2020-05-18 04:35:25 -0600

    Adding wildcard servlet path example.

--- src/main/java/servlet/HelloServlet.java
+++ src/main/java/servlet/HelloServlet.java
@@ -11,7 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet(
         name = "MyServlet", 
-        urlPatterns = {"/hello"}
+        urlPatterns = {"/hello/*"}
     )
 public class HelloServlet extends HttpServlet {
 
